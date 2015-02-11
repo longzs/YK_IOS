18135e3cf23bd57677591b2382eb5b3b13374bad
J219 <end1988@126.com>
2015-02-11 17:16:54 +0800

    配置完成页面

---
+++ YueKong/ViewControllers/ConfigFinishViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfigFinishViewController.h
+//  YueKong
+//
+//  Created by WangXun on 15/2/11.
+//  Copyright (c) 2015年 yuekong. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ConfigFinishViewController : BaseViewController
+
+@end
